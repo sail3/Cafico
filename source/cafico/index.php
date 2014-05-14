4ef88779ea314031bbd476c5fb6ef259cf4cf97f
Pedro Charun <mlosail3@gmail.com>
2014-05-14 15:33:08 -0500

    Pagina principal del proyecte imagenes.

---
+++ source/cafico/index.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Cafico Peru</title>
+		<link rel="stylesheet" href="css/styles.css">
+	</head>
+	<body>
+		<div id="wrapper">
+			<header>
+				<div class="logo"><img src="images/logo.jpg" alt="Cafico peru"></div>
+				<ul class="menu-header">
+					<li class="menu-header-first"><a href="javascript:">INICIO</a></li>
+					<li><a href="javascript:">FINANCIERO</a></li>
+					<li><a href="javascript:">INMOBILIARIO</a></li>
+					<li><a href="javascript:">COMERCIAL</a></li>
+					<li><a href="javascript:">CONTACTENOS</a></li>
+				</ul>
+				<div class="social-media">
+					<div class="sm-item">
+						<div class="facebook">
+							<a href="http://facebook.com">
+								<img src="images/social-icons.png" alt="Facebook">
+							</a>
+						</div>
+						<p>VISITANOS</p>
+					</div>
+				</div>
+			</header>
+			<section>
+				<div class="slide-show">
+					<div class="slide"><img src="images/slide-image.jpg" alt="imagen slideshow"></div>
+				</div>
+			</section>
+		</div>
+		<section class="main-content">
+			<div class="wrapper">
+				<article class="main-page">
+					<div class="content">
+						<h2>BIENVENIDOS</h2>
+						<div>
+							<p>
+							Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+								
+							</p>			
+						</div>
+					</div>
+				</article>
+			</div>
+			<div class="slogan">
+				<h1>Centro de Arbitraje Financiero Inmobiliario Comercial del Perú</h1>
+			</div>
+		</section>
+		<footer>
+			<div class="wrapper">
+				<div class="info">
+					<p>TELEFONO: 3216546531<br>
+					NEXTEL: 32165463213<br>
+					E-MAIL: secretaria@cofiperu.com<br>
+					DIRECCION: Calle Diez Canseco N° 442, OFICINA 202, MIRAFLORES
+					</p>
+				</div>
+				<div class="social-media">
+					<div class="sm-item">
+						<div class="facebook">
+							<a href="http://facebook.com">
+								<img src="images/social-icons.png" alt="Facebook">
+							</a>
+						</div>
+						<p>VISITANOS</p>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</body>
+</html>
